11535b4f81300d308425964b9240e6ee6abb6428
DimMilios <elles1992@yahoo.gr>
2022-06-19 10:37:24 +0300

    preparing for heroku deployment

--- verovio-rest-api/src/routes/api/comments.ts
+++ verovio-rest-api/src/routes/api/comments.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import { validationResult } from 'express-validator';
 import prisma from '../../config/db-client';
 import { requireAuth } from '../../middleware/auth';
-import { commentCreateRules, commentDeleteRules } from '../../middleware/validators';
-import { notifySubs, subs } from './events';
+import {
+  commentCreateRules,
+  commentDeleteRules,
+} from '../../middleware/validators';
+// import { notifySubs, subs } from './events';
 
 export const router = express.Router();
 
@@ -35,27 +38,35 @@ router.post('/', requireAuth, ...commentCreateRules, async (req: any, res) => {
         // parentCommentId,
         multiSelectElements,
         usersDocuments: { create: { userId, documentId } },
-        parentComment: !parentCommentId ? undefined : { connect: { id: parentCommentId} }
+        parentComment: !parentCommentId
+          ? undefined
+          : { connect: { id: parentCommentId } },
       },
       include: {
         usersDocuments: {
-          select: { user: true, commentId: true, documentId: true, createdAt: true }
+          select: {
+            user: true,
+            commentId: true,
+            documentId: true,
+            createdAt: true,
+          },
         },
-      }
+      },
     });
 
     console.log('Created comment', createdComment);
 
     res.status(201).json(createdComment);
 
-    notifySubs(subs, clientId, documentId, { type: 'comments:create', createdComment });
+    // notifySubs(subs, clientId, documentId, { type: 'comments:create', createdComment });
   } catch (error) {
     console.log(error);
     res.status(400).json('Something went wrong');
   }
 });
 
-router.delete('/:commentId',
+router.delete(
+  '/:commentId',
   requireAuth,
   ...commentDeleteRules,
   async (req, res, next) => {
@@ -67,46 +78,63 @@ router.delete('/:commentId',
 
     const { userId, documentId, clientId } = req.body;
     const commentId = req?.params?.commentId;
-    console.log('DELETE /api/comments/:commentId', { commentId, userId, documentId, clientId, sessionUser: req.user });
+    console.log('DELETE /api/comments/:commentId', {
+      commentId,
+      userId,
+      documentId,
+      clientId,
+      sessionUser: req.user,
+    });
 
     try {
       const result = await prisma.comment.delete({
-        where: { id: Number(commentId) }
+        where: { id: Number(commentId) },
       });
 
       if (!result) {
-        return res.status(400).json({ error: `Could not delete comment with id ${commentId}`});
+        return res
+          .status(400)
+          .json({ error: `Could not delete comment with id ${commentId}` });
       }
 
-      notifySubs(subs, clientId, documentId, { type: 'comments:delete', ids: [commentId] });
-      
-      return res.status(200).json({ msg: `Comment of id: ${commentId} has been deleted` });
-      
+      // notifySubs(subs, clientId, documentId, { type: 'comments:delete', ids: [commentId] });
+
+      return res
+        .status(200)
+        .json({ msg: `Comment of id: ${commentId} has been deleted` });
     } catch (error) {
       console.log('Could not delete comment with id ', commentId, error);
       next(error);
     }
-  });
-  
-  router.delete('/', requireAuth, async (req, res, next) => {
-    const { documentId, clientId } = req.body;
-    
-    try {
-      const doc = await prisma.document.findFirst({
-        where: { id: Number(documentId) },
-        select: { usersComments: { select: { commentId: true } } },
-      });
+  }
+);
 
-      await prisma.comment.deleteMany({
-        where: { usersDocuments: { every: { documentId: Number(documentId) } } },
-      });
-    
-    notifySubs(subs, clientId, documentId, { type: 'comments:delete', ids: doc?.usersComments.map(uc => uc.commentId) });
-      
-    return res.status(200).json({ msg: `Comments for document with id: ${documentId} have been deleted` });
+router.delete('/', requireAuth, async (req, res, next) => {
+  const { documentId, clientId } = req.body;
 
+  try {
+    const doc = await prisma.document.findFirst({
+      where: { id: Number(documentId) },
+      select: { usersComments: { select: { commentId: true } } },
+    });
+
+    await prisma.comment.deleteMany({
+      where: { usersDocuments: { every: { documentId: Number(documentId) } } },
+    });
+
+    // notifySubs(subs, clientId, documentId, { type: 'comments:delete', ids: doc?.usersComments.map(uc => uc.commentId) });
+
+    return res
+      .status(200)
+      .json({
+        msg: `Comments for document with id: ${documentId} have been deleted`,
+      });
   } catch (error) {
-    console.log('Could not delete comments for document with id', documentId, error);
+    console.log(
+      'Could not delete comments for document with id',
+      documentId,
+      error
+    );
     next(error);
   }
-})
+});
