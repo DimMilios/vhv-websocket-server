21553e06ffffab095807306507f99dcf5c02c105
DimMilios <elles1992@yahoo.gr>
2022-06-26 19:52:02 +0300

    replace localhost URL references to current location origin

--- verovio-rest-api/public/js/index.js
+++ verovio-rest-api/public/js/index.js
@@ -1,3 +1,5 @@
+const baseUrl = window.location.origin;
+
 let createDocumentForm = document.querySelector('#new-document');
 
 let documentList = document.querySelector('#document-list');
@@ -19,7 +21,10 @@ documentList?.addEventListener('click', event => {
     console.log({ currentElem, docId, doctitle });
 
     if (doctitle) {
-      window.open(`http://localhost:3001/client?docId=${docId}&roomname=${doctitle}`, '_blank');
+      window.open(
+        `${baseUrl}/client?docId=${docId}&roomname=${doctitle}`,
+        '_blank'
+      );
     }
   }
 });
@@ -28,21 +33,18 @@ let deleteButtons = document.querySelectorAll('.document-delete');
 deleteButtons.forEach(btn => {
   btn.addEventListener('click', async event => {
     let currentElem = event.target.closest('tr');
-    console.log(`Clicked delete button ${currentElem}`)
+    console.log(`Clicked delete button ${currentElem}`);
 
     if (currentElem) {
       let docId = Number(currentElem.dataset.docId);
-      let response = await fetch(
-        `http://localhost:3001/api/documents/${docId}`,
-        {
-          method: 'DELETE',
-        }
-      );
+      let response = await fetch(`${baseUrl}/api/documents/${docId}`, {
+        method: 'DELETE',
+      });
 
       console.log(response);
 
       if (response.status === 200) {
-        window.location = "http://localhost:3001/dashboard";
+        window.location = `${baseUrl}/dashboard`;
       }
     }
   });
@@ -54,44 +56,48 @@ inviteCollabForm?.addEventListener('submit', async event => {
 
   let documentId = Number(event.target.dataset.sharedDocumentId);
   let email = new FormData(event.target).get('email');
-  let response = await fetch(
-    `http://localhost:3001/api/users/invite/${email}`,
-    {
-      method: 'POST',
-      body: JSON.stringify({ documentId }),
-      headers: { 'Content-Type': 'application/json' },
-    }
-  );
+  let response = await fetch(`${baseUrl}/api/users/invite/${email}`, {
+    method: 'POST',
+    body: JSON.stringify({ documentId }),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
   $('#invite-collaborator-modal').modal('hide');
-  
+
   if (response.ok) {
     let data = await response.json();
-    
+
     navigator.clipboard
-    .writeText(data.inviteURL)
-    .then(() => {
-      console.log(data.inviteURL, 'written to clipboard');
-      let notification = document.createElement('div');
-      document.body.firstElementChild.appendChild(notification);
-      notification.outerHTML = createAlert('Success!', 'Invitation URL has been copied to your clipboard.');
-    })
-    .catch(err => console.log(err));
+      .writeText(data.inviteURL)
+      .then(() => {
+        console.log(data.inviteURL, 'written to clipboard');
+        let notification = document.createElement('div');
+        document.body.firstElementChild.appendChild(notification);
+        notification.outerHTML = createAlert(
+          'Success!',
+          'Invitation URL has been copied to your clipboard.'
+        );
+      })
+      .catch(err => console.log(err));
   } else {
     let notification = document.createElement('div');
     document.body.firstElementChild.appendChild(notification);
-    notification.outerHTML = createAlert('Error!', 'Could not create invitation URL.', 'danger');
+    notification.outerHTML = createAlert(
+      'Error!',
+      'Could not create invitation URL.',
+      'danger'
+    );
   }
   inviteCollabForm.reset();
 });
 
-function createAlert(strong = '', message = '', type = "success") {
+function createAlert(strong = '', message = '', type = 'success') {
   return `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
     <strong>${strong}</strong> ${message}
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
-  </div>`
+  </div>`;
 }
 
 let shareButtons = document.querySelectorAll('.document-share');
@@ -108,15 +114,15 @@ let docUpdateForms = document.querySelectorAll('.edit-doc-form');
 docUpdateForms.forEach(form => {
   form.addEventListener('submit', async event => {
     event.preventDefault();
-    
+
     let but = event.target.querySelector('input');
     but.type = but.type === 'search' ? 'text' : 'search';
-    
+
     let docId = event.target.id.match(/(\d+)/)[0];
-    
+
     console.log(`Submitted document update form for document: ${docId}`);
 
-    let response = await fetch(`http://localhost:3001/api/documents/${docId}`, {
+    let response = await fetch(`${baseUrl}/api/documents/${docId}`, {
       method: 'PUT',
       credentials: 'include',
       body: JSON.stringify({
